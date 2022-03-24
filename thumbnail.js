f6bed183fc4933ccbfa1263e174d578b487179a2
MMG956 <67861460+MMG956@users.noreply.github.com>
2022-03-24 23:31:03 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,4 +3,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
     targetImage.addEventListener("click", function() {
         targetImage.classList.remove("small")
     });
+    let AIMG = document.querySelector("#A-image");
+    AIMG.addEventListener("click", function() {
+        AIMG.classList.remove("small")
+    });
+    let BIMG = document.querySelector("#B-image");
+    BIMG.addEventListener("click", function() {
+        BIMG.classList.remove("small")
+    });
+    let CIMG = document.querySelector("#C-image");
+    CIMG.addEventListener("click", function() {
+        CIMG.classList.remove("small")
+    });
+    let DIMG = document.querySelector("#D-image");
+    DIMG.addEventListener("click", function() {
+        DIMG.classList.remove("small")
+    });
+    let EIMG = document.querySelector("#E-image");
+    EIMG.addEventListener("click", function() {
+        EIMG.classList.remove("small")
+    });
+    let FIMG = document.querySelector("#F-image");
+    FIMG.addEventListener("click", function() {
+        FIMG.classList.remove("small")
+    });
+    let GIMG = document.querySelector("#G-image");
+    GIMG.addEventListener("click", function() {
+        GIMG.classList.remove("small")
+    });
+    let HIMG = document.querySelector("#H-image");
+    HIMG.addEventListener("click", function() {
+        HIMG.classList.remove("small")
+    });
+    let IMG = document.querySelector("#I-image");
+    FIMG.addEventListener("click", function() {
+        IIMG.classList.remove("small")
+    });
+    let JIMG = document.querySelector("#J-image");
+    JIMG.addEventListener("click", function() {
+        JIMG.classList.remove("small")
+    });
+    let KIMG = document.querySelector("#K-image");
+    FIMG.addEventListener("click", function() {
+        KIMG.classList.remove("small")
+    });
+    let LIMG = document.querySelector("#L-image");
+    LIMG.addEventListener("click", function() {
+        LIMG.classList.remove("small")
+    });
+    let MIMG = document.querySelector("#M-image");
+    MIMG.addEventListener("click", function() {
+        MIMG.classList.remove("small")
+    });
 });
